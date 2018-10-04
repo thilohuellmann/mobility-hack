ed0d87f8bc7123c671f32e0ae65ca8e5971284e1
Thilo Huellmann <thilohuellmann@Thilos-MacBook-Pro.local>
2018-10-04 21:23:55 +0200

    created views, templates and url paths (routing)

--- mobility/views.py
+++ mobility/views.py
@@ -10,6 +10,108 @@ import os
 boto_key = os.environ.get("BOTO_PUB_KEY")
 boto_s_key = os.environ.get("BOTO_SECRET_KEY")
 
-@login_required
+
 def index(request):
     return render(request, 'mobility/index.html')
+
+# # # # # # # # # # # # # # #
+#                           #
+#  SENIORS' VIEWS BEGIN     #
+#                           #
+# # # # # # # # # # # # # # #
+
+@login_required
+# @is_senior <-- implement decorator
+def create_profile_senior(request):
+    return render(request, 'mobility/create_profile_senior.html', context={})
+
+@login_required
+# @is_senior
+def profile_senior(request):
+    return render(request, 'mobility/profile_senior.html', context={})
+
+@login_required
+# @is_senior
+def my_trips_senior(request):
+    return render(request, 'mobility/my_trips_senior.html', context={})
+
+@login_required
+# @is_senior
+def trip_details_senior(request):
+    return render(request, 'mobility/trip_details_senior.html', context={})
+
+@login_required
+# @is_senior
+def trip_create_1_senior(request):
+    return render(request, 'mobility/trip_create_1_senior.html', context={})
+
+@login_required
+# @is_senior
+def trip_create_2_senior(request):
+    return render(request, 'mobility/trip_create_2_senior.html', context={})
+
+@login_required
+# @is_senior
+def trip_create_3_senior(request):
+    return render(request, 'mobility/trip_create_3_senior.html', context={})
+
+@login_required
+# @is_senior
+def trip_create_4_1_senior(request):
+    return render(request, 'mobility/trip_create_4_1_senior.html', context={})
+
+@login_required
+# @is_senior
+def trip_create_4_2_1_senior(request):
+    return render(request, 'mobility/trip_create_4_2_1_senior.html', context={})
+
+@login_required
+# @is_senior
+def trip_create_4_2_2_senior(request):
+    return render(request, 'mobility/trip_create_4_2_2_senior.html', context={})
+
+@login_required
+# @is_senior
+def trip_create_5_senior(request):
+    return render(request, 'mobility/trip_create_5_senior.html', context={})
+
+@login_required
+# @is_senior
+def trip_creation_confirmation_senior(request):
+    return render(request, 'mobility/trip_creation_confirmation_senior.html', context={})
+
+# # # # # # # # # # # # # # #
+#                           #
+#  SUPPORTERS' VIEWS BEGIN  #
+#                           #
+# # # # # # # # # # # # # # #
+
+@login_required
+# @is_supporter
+def create_profile_supporter(request):
+    return render(request, 'mobility/create_profile_supporter.html', context={})
+
+@login_required
+# @is_supporter
+def profile_supporter(request):
+    return render(request, 'mobility/profile_supporter.html', context={})
+
+@login_required
+# @is_supporter
+def discover_trips_supporter(request):
+    return render(request, 'mobility/discover_trips_supporter.html', context={})
+
+@login_required
+# @is_supporter
+def my_trips_supporter(request):
+    return render(request, 'mobility/my_trips_supporter.html', context={})
+
+@login_required
+# @is_supporter
+def trip_details_supporter(request):
+    return render(request, 'mobility/trip_details_supporter.html', context={})
+
+@login_required
+# @is_supporter
+def trip_application_confirmation_supporter(request):
+    return render(request, 'mobility/trip_application_confirmation_supporter.html', context={})
