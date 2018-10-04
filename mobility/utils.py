3701e96c21a6af953d5ac1ceff48af32f0d8f162
Thilo Huellmann <thilohuellmann@Thilos-MacBook-Pro.local>
2018-10-04 16:17:33 +0200

    first migration

--- mobility/utils.py
+++ mobility/utils.py
@@ -3,16 +3,16 @@ import os
 from django.db import models
 from django.db.models import F
 from django.contrib.auth import get_user_model
+import boto
 
 User = get_user_model()
 
 boto_key = os.environ.get("BOTO_PUB_KEY")
 boto_s_key = os.environ.get("BOTO_SECRET_KEY")
 
-conn = boto.connect_s3(boto_key, boto_s_key)
-bucket = conn.get_bucket('mobility')
-k = Key(bucket)
-
+# conn = boto.connect_s3(boto_key, boto_s_key)
+# bucket = conn.get_bucket('mobility-hack-new')
+# k = Key(bucket)
 
 def s3_delete(user_id, file_name):
     k.key = '/datasets/user_{0}/{1}'.format(user_id, file_name)
