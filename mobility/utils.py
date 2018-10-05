ba2695daaccb4634c66f8ceab3881beb3fdfa2e2
Christoph Haarburger <christoph.haarburger@lfb.rwth-aachen.de>
2018-10-05 10:08:42 +0200

    fix averaging even cooler 🤪

--- mobility/utils.py
+++ mobility/utils.py
@@ -59,7 +59,9 @@ def haversine(lng_1, lat_1, lng_2, lat_2):
 def average_rating(user_id):
     ratings = Rating.objects.filter(user_id=user_id)
 
-    if len(ratings) == 0:
+    if not ratings:
+        return 3.1
+    elif len(ratings) == 0:
         return 3.1
     else:
         rating_sum = 0
