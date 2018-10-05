a92b2c3a3e69f1fc4e1c9362fd15a6c1e7b29cac
Jannis Fleer <fleerjannis@aol.com>
2018-10-05 08:21:40 +0200

    layout.htmls

--- accounts/views.py
+++ accounts/views.py
@@ -6,7 +6,7 @@ from accounts.models import User
 
 # create your views here
 
-def SignUp(request):
+def SeniorSignUp(request):
     if request.method == 'POST':
         form = SignUpForm(request.POST)
         if form.is_valid():
@@ -20,11 +20,34 @@ def SignUp(request):
             today = now.date()
             next_month = today + datetime.timedelta(days=+30)
 
-            return redirect('index')
+            return redirect('my_trips_senior')
             # return HttpResponseRedirect('/')
 
     else:
         # show message
         form = SignUpForm()
-        
-    return render(request, 'signup.html', {'form': form})
+
+    return render(request, 'senior_signup.html', {'form': form})
+
+def SupporterSignUp(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('email')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            # save subscription info in backend
+            login(request, user)
+            now = datetime.datetime.now()
+            today = now.date()
+            next_month = today + datetime.timedelta(days=+30)
+
+            return redirect('my_trips_supporter')
+            # return HttpResponseRedirect('/')
+
+    else:
+        # show message
+        form = SignUpForm()
+
+    return render(request, 'supporter_signup.html', {'form': form})
